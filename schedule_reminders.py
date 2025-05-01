99749bdbdb9ab67bbad0e77f47a9b13bb9303333
HarveyDWilliams <36428594+HarveyDWilliams@users.noreply.github.com>
2025-05-01 20:28:30 +0100

    Rolling back to call every minute with additional last call logic.

--- schedule_reminders.py
+++ schedule_reminders.py
@@ -1,38 +1,8 @@
 import pymongo
 import credential_manager as cm
-import sys
 import datetime
 import logging
-from subprocess import run
-
-
-def schedule_reminder(phone_number: str, time: str):
-    """
-    Uses the linux at command to schedule the reminder python script.
-
-    Arguments:
-        phone_number (str): phone number to send the reminder to
-        time (str): time at which to send the reminder
-    """
-
-    logging.info(f'/home/ubuntu/pluswordchatbot/send_reminder.sh {phone_number} | at {time}')
-    run(f'/home/ubuntu/pluswordchatbot/send_reminder.sh {phone_number} | at {time}', shell=True)
-
-
-def set_reminders(reminders):
-    """
-    Iterates through reminders scheduling a reminder for each.
-    """
-
-    if not reminders:
-        return
-
-    for phone_number, data in reminders.items():
-        if data.get("enabled"):
-            schedule_reminder(
-                phone_number,
-                data.get("reminder_time")
-            )
+import requests
 
 
 def get_reminders() -> [{str: str}]:
@@ -74,10 +44,74 @@ def get_reminders() -> [{str: str}]:
     return reminder_data
 
 
+def send_reminder(phone_number):
+    """
+    Sends reminder to the player.
+
+    Arguments:
+        phone_number: the phone number of the player to remind
+    """
+
+    header = {
+        "Authorization": f"Bearer {cm.get_whatsapp_key()}"
+    }
+
+    url = f"https://graph.facebook.com/v21.0/{cm.get_whatsapp_page_id()}/messages"
+
+    body = {
+        "messaging_product": "whatsapp",
+        "recipient_type": "individual",
+        "to": phone_number,
+        "type": "text",
+        "text": {
+            "preview_url": True,
+            "body": "nice ones all so far"
+        }
+    }
+    requests.post(url=url, json=body, headers=header)
+    logging.info(f"Sent reminder to {phone_number}.")
+
+
+def check_if_valid_reminder(phone_number: str):
+    """
+    Check the DB for two conditions:
+        1) Has the player got a reminder set.
+        2) Has the player failed to submit a time today.
+
+    If both are true then we message the player to remind them to submit.
+
+    Arguments:
+        phone_number: the phone number of the player to remind
+    """
+
+    today_date = datetime.date.today()
+    today_start = datetime.datetime(today_date.year, today_date.month, today_date.day, 0, 0, 0)
+
+    client = pymongo.MongoClient(cm.get_db_connection_string())
+
+    reminders = client["PlusWord"]["Reminders"]
+    player_reminder = reminders.find_one({"$and": [{"enabled": True}, {"phone_number": phone_number}]})
+
+    submitted = client["PlusWord"]["Times"]
+    player_submission = submitted.find_one(
+        {"$and": [{"load_ts": {'$gte': today_start}}, {"phone_number": phone_number}]})
+
+    if player_submission:
+        return False
+
+    if not player_reminder:
+        return False
+
+    return True
+
+
 def main():
     logging.basicConfig(filename="reminder_log.log", level=logging.INFO)
     reminders = get_reminders()
-    set_reminders(reminders)
+    for phone_number, values in reminders.items():
+        if check_if_valid_reminder(phone_number) and values.get('enabled'):
+            if datetime.datetime.now().strftime('%H:%M') == values.get('reminder_time'):
+                send_reminder(phone_number)
 
 
 if __name__ == "__main__":
